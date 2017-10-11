eaa1a7bf20e9567d0d89f2590bc887580fdff130
vagrant <thecarloferrer@gmail.com>
2017-10-11 23:50:23 +0000

    Fixed the sign up and login pages so that they don't look like ass.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      byebug
       redirect_to :root
     else
       redirect_to '/login'
