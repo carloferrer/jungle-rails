fe15bd6f71953bdfe8ad82f6f8d0b61185cfe68b
vagrant <thecarloferrer@gmail.com>
2017-10-11 22:05:07 +0000

    Started implementation of login session.

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,8 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-  resources :users, only: [:create, :new]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
