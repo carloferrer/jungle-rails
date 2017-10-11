4862008e7820350870f18a51d1a16257699da9e6
vagrant <thecarloferrer@gmail.com>
2017-10-11 19:43:53 +0000

    Created admin/categories_controller.rb similar to admin/products_controller.rb.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,24 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all #order categories by descending ID
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category craeted!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
