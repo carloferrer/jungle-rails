fe6d03ada01f615f83e607aeaac108f1ed4c6e3c
vagrant <thecarloferrer@gmail.com>
2017-10-11 22:20:25 +0000

    Did just enough work to allow user accounts to be created. Can't do much else other than that though.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
 
-def new
-end
-
-def create
-  user = User.new(user_params)
-  if user.save
-    session[:user_id] = user.id
-    redirect_to '/'
-  else
-    redirect_to '/signup'
+  def new
   end
-end
 
-def login
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      byebug
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+      byebug
+    end
+  end
 
-private
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  def login
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 
 end
 
