588651c5c56f340f541624b55de5a5b65eee3c50
vagrant <thecarloferrer@gmail.com>
2017-10-12 22:44:15 +0000

    Can reach the correct 'raise' error message.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+  def create
+    raise "Hello - from, Review."
+    # @user_id = session[:user_id]
+
+    # @review = Review.new(review_params)
+  end
+
+  # def review_params
+  #   params.require(:review).permit(:user_id, :product_id, :description, :rating)
+  # end
+
 end
