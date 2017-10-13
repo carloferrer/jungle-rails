dc9bc31f39c85168746209211e57436ae2cec8e1
vagrant <thecarloferrer@gmail.com>
2017-10-13 00:03:22 +0000

    Reviews can be saved now.  Hurrah!

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,21 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Hello - from, Review."
+    # raise "Hello - from, Review."
     # @user_id = session[:user_id]
+    # byebug
+    @user_id = current_user.id
+    @product_id = params['product_id']
+    @description = review_params['description']
+    @rating = review_params['rating']
 
-    # @review = Review.new(review_params)
+
+    @review = Review.create!({product_id: @product_id, user_id: @user_id, description: @description, rating: @rating})
+    redirect_to :root
   end
 
-  # def review_params
-  #   params.require(:review).permit(:user_id, :product_id, :description, :rating)
-  # end
+  def review_params
+    params.require(:review).permit(:product_id, :user_id, :description, :rating)
+  end
 
 end
