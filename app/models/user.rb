2dbf05b39efe50d8220397507d2f469c30276083
vagrant <thecarloferrer@gmail.com>
2017-10-16 21:17:32 +0000

    Minimum password length required - as well, user_spec updated to test that validation passes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   has_many :reviews
 
   has_secure_password
+  validates :password, length: { minimum: 1 }
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: {:case_sensitive => false}
   validates :first_name, presence: true
   validates :last_name, presence: true
 
