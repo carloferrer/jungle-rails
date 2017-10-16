f32b3b8dab4328f0a3ca6d7ddf3aef9de56a33e8
vagrant <thecarloferrer@gmail.com>
2017-10-16 17:01:01 +0000

    Rando commit to exit branch.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :reviews
 
   has_secure_password
-  validates :email, uniqueness: true
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
 end
